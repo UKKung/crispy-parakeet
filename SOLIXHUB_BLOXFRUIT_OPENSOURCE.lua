d0a2da4716cd3229c515bea56d8dfa6974d01539
UKKung <102902536+UKKung@users.noreply.github.com>
2026-02-27 18:58:57 -0800

    Update and rename SOLIXHUB_BLOXFRUIT_OPENSOURCE.txt to SOLIXHUB_BLOXFRUIT_OPENSOURCE.lua

--- SOLIXHUB_BLOXFRUIT_OPENSOURCE.lua
+++ SOLIXHUB_BLOXFRUIT_OPENSOURCE.lua
@@ -1,6 +1,8 @@
 --[[
 	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
 ]]
+
+-- loadstring(game:HttpGet('https://raw.githubusercontent.com/UKKung/crispy-parakeet/refs/heads/main/SOLIXHUB_BLOXFRUIT_OPENSOURCE.txt'))()
 repeat wait() until game:IsLoaded()
 
 local wait = task.wait
